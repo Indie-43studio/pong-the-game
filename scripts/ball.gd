acdb22fd2b91c45577b1fdaca68b1ca5a4d5d8c9
Indie-43studio <IndieGame43studio@gmail.com>
2025-06-12 01:10:00 +0800

    6/12
    finish the basement of this project

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,10 +1,19 @@
 extends CharacterBody2D
 
+@onready var audio_player: AudioStreamPlayer = $BallAudioStreamPlayer
 @export var speed: float = 400.0
+@export var paddle: AudioStream
+@export var wall: AudioStream
+@export var score: AudioStream
+@export var serve: AudioStream
+@export var game_end: AudioStream
+
 var screen_size: Vector2
 var max_bounce_angle = deg_to_rad(60)
 var current_speed : float = speed
 var serve_direction : Vector2
+var game_end_sound_played: bool = false
+
 func _ready():
 	add_to_group("ball")
 	GameManager.connect("ready_serve",new_serve)
@@ -20,16 +29,26 @@ func _physics_process(delta):
 		var collider = collision.get_collider()
 
 		if collider.is_in_group("paddle"):
+			audio_player.stream = paddle
 			var offset_y = global_position.y - collider.global_position.y
 			var normalized_offset = offset_y / (collider.get_node("CollisionShape2D").shape.height/2)
 			var new_angle = normalized_offset * max_bounce_angle
 			current_speed+=10
 			velocity = Vector2(-sign(velocity.x) * cos(new_angle), sin(new_angle)).normalized() * current_speed
 		else:
+			audio_player.stream = wall
 			velocity = velocity.bounce(collision.get_normal())
+		audio_player.play()
+	if GameManager.is_game_over and not game_end_sound_played:
+		audio_player.stream = game_end
+		audio_player.play()
+		game_end_sound_played = true
+	
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	scale = Vector2(0.01,0.01)
+	audio_player.stream = score
+	audio_player.play()
 	if position.x < (screen_size.x/2):
 		GameManager.score_point("player_right")
 	else:
@@ -51,5 +70,8 @@ func new_serve(scoring_player: String):
 		.set_ease(Tween.EASE_IN)\
 		.set_trans(Tween.TRANS_QUAD)
 	await tween.finished 
+	audio_player.stream = serve
+	audio_player.play()
+	await audio_player.finished
 	velocity = serve_direction.rotated(randf_range(-0.25, 0.25)) * speed
 	
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,9 +1,10 @@
 extends CanvasLayer
-
+@export var audio_player: AudioStreamPlayer
 @export var choose_game_mode: Window
 
 func _on_start_pressed() -> void:
 	choose_game_mode.show()
+	audio_player.play()
 
 func _on_window_close_requested() -> void:
 	choose_game_mode.hide()
@@ -12,12 +13,16 @@ func _on_quit_pressed() -> void:
 	get_tree().quit()
 
 func _on_vs_ai_pressed() -> void:
+	audio_player.play()
+	await audio_player.finished
 	if GameManager:
 		GameManager.set_game_mode("AI")
 		get_tree().change_scene_to_file("res://scences/main.tscn")
 		GameManager.ready_serve.emit("player_left")
 
 func _on_vs_player_pressed() -> void:
+	audio_player.play()
+	await audio_player.finished
 	if GameManager:
 		GameManager.set_game_mode("2P")
 		get_tree().change_scene_to_file("res://scences/main.tscn")
